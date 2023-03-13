9573fe607e409b08dac0a027758a19a748039e95
vico <vicol2023@student.cis.edu.hk>
2023-03-13 14:45:59 +0800

    push

--- src/DominoPile.java
+++ src/DominoPile.java
@@ -1,7 +1,60 @@
+import java.util.ArrayList;
+
 public class DominoPile {
 
+    private ArrayList<Domino> pile = new ArrayList<Domino>();
 
-    public void getPile() {
+    public DominoPile() {
+
+        this.pile = pile;
+    }
+
+    public ArrayList<Domino> getPile() {
+        return pile;
+    }
+
+    public void setPile(ArrayList<Domino> pile) {
+        this.pile = pile;
+    }
+
+    public void newStack6()
+    {
+        for(int i = 0; i < 7; i++)
+        {
+            for(int k=0; k < 7; k++)
+            {
+                if (pile.isEmpty())
+                {
+                    Domino newDom = new Domino(k, i);
+                    pile.add(newDom);
+                }
+                else
+                {
+                    System.out.println(pile);
+                    for (Domino domino : pile)
+                    {
+                        int currTop = domino.getTop();
+                        int currBottom = domino.getBottom();
+                        System.out.println(currTop);
+                        System.out.println(currBottom);
+                        if (i == currTop && k == currBottom) {
+
+                        } else if (k == currTop && i == currBottom) {
+
+                        } else {
+                            Domino newDom = new Domino(k, i);
+//                            pile.add(newDom);
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(pile.size());
+    }
+
+    public void shuffle()
+    {
 
     }
+
 }
